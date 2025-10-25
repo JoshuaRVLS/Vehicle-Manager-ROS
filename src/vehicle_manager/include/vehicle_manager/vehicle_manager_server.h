10a88e363325fa4956049f930122d35bcf40a71b
JoshuaRVLS <jravaellnew@gmail.com>
2025-10-25 09:15:26 +0700

    Add EditVehicle and DeleteVehicle method declarations to VehicleManagerServer

--- src/vehicle_manager/include/vehicle_manager/vehicle_manager_server.h
+++ src/vehicle_manager/include/vehicle_manager/vehicle_manager_server.h
@@ -3,6 +3,8 @@
 #include "vehicle_manager/RegisterVehicle.h"
 #include "vehicle_manager/DisplayAllVehicle.h"
 #include "vehicle_manager/FindVehicle.h"
+#include "vehicle_manager/EditVehicle.h"
+#include "vehicle_manager/DeleteVehicle.h"
 #include <vector>
 #include <iostream>
 
@@ -15,7 +17,8 @@ class VehicleManagerServer {
   bool register_vehicle(vehicle_manager::RegisterVehicle::Request& req, vehicle_manager::RegisterVehicle::Response& res);
   bool display_all_vehicles(vehicle_manager::DisplayAllVehicle::Request& req, vehicle_manager::DisplayAllVehicle::Response& res);
   bool find_vehicle(vehicle_manager::FindVehicle::Request& req, vehicle_manager::FindVehicle::Response& res);
-
+  bool edit_vehicle(vehicle_manager::EditVehicle::Request& req, vehicle_manager::EditVehicle::Response& res);
+  bool delete_vehicle(vehicle_manager::DeleteVehicle::Request& req, vehicle_manager::DeleteVehicle::Response& res);
 public:
   VehicleManagerServer();
 
