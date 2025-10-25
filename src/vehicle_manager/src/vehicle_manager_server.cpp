192b40fea278b06fbdc3a93837b444beba607125
JoshuaRVLS <jravaellnew@gmail.com>
2025-10-25 09:15:35 +0700

    Add edit and delete vehicle functionalities to VehicleManagerServer

--- src/vehicle_manager/src/vehicle_manager_server.cpp
+++ src/vehicle_manager/src/vehicle_manager_server.cpp
@@ -1,6 +1,5 @@
 #include "vehicle_manager/vehicle_manager_server.h"
 
-
 VehicleManagerServer::VehicleManagerServer() {};
 
 bool VehicleManagerServer::register_vehicle(vehicle_manager::RegisterVehicle::Request & req, vehicle_manager::RegisterVehicle::Response & res)
@@ -14,6 +13,7 @@ bool VehicleManagerServer::register_vehicle(vehicle_manager::RegisterVehicle::Re
     ROS_ERROR("Cant add new vehicle! Vehicle exists!");
     return true;
   }
+  req.vehicle.id = vehicles.size();
   vehicles.push_back(req.vehicle);
   res.message = "Mobil Baru dengan plat " + req.vehicle.licensePlate + " Berhasil ditambahkan";
   res.success = true;
@@ -56,11 +56,61 @@ bool VehicleManagerServer::find_vehicle(vehicle_manager::FindVehicle::Request & 
   return true;
 }
 
-void VehicleManagerServer::start() {
+bool VehicleManagerServer::edit_vehicle(vehicle_manager::EditVehicle::Request &req, vehicle_manager::EditVehicle::Response &res)
+{
+  auto it = find_if(vehicles.begin(), vehicles.end(),
+            [&req](const vehicle_manager::Vehicle& v) { return v.id == req.vehicle_id; });
+
+  if (it == vehicles.end()) {
+    res.success = false;
+    res.message = "ID tidak ditemukan!";
+    ROS_ERROR("Update vehicle dengan license plate %s gagal", it->licensePlate.c_str());
+    return true;
+  }
+
+  if (req.property == "licensePlate") {
+    it->licensePlate = req.new_value;
+  } else if (req.property == "make") {
+    it->make = req.new_value;
+  } else if (req.property == "model") {
+    it->model = req.new_value;
+  } else if (req.property == "year") {
+    it->year = req.new_value;
+  }
+
+  ROS_INFO("Update vehicle dengan license plate %s gagal", it->licensePlate.c_str());
+  res.success = true;
+  res.message = "Update berhasil!";
+  return true;
+}
+
+bool VehicleManagerServer::delete_vehicle(vehicle_manager::DeleteVehicle::Request &req, vehicle_manager::DeleteVehicle::Response &res)
+{
+  auto it = find_if(vehicles.begin(), vehicles.end(),
+            [&req](const vehicle_manager::Vehicle& v) { return v.id == req.vehicle_id; });
+
+  if (it == vehicles.end()) {
+    res.success = false;
+    res.message = "ID tidak ditemukan!";
+    ROS_ERROR("Delete vehicle dengan license plate %s gagal", it->licensePlate.c_str());
+    return true;
+  }
+
+  res.message = "Vehicle dengan plat nomor " + it->licensePlate + " berhasil di hapus!";
+  res.success = true;
+  vehicles.erase(it);
+  return true;
+}
+
+void VehicleManagerServer::start()
+{
 
   ros::ServiceServer register_vehicle_service = nh.advertiseService("register_vehicle_service", &VehicleManagerServer::register_vehicle, this);
   ros::ServiceServer display_all_vehicles = nh.advertiseService("display_all_vehicles", &VehicleManagerServer::display_all_vehicles, this);
   ros::ServiceServer find_vehicle = nh.advertiseService("find_vehicle", &VehicleManagerServer::find_vehicle, this);
+  ros::ServiceServer edit_vehicle = nh.advertiseService("edit_vehicle", &VehicleManagerServer::edit_vehicle, this);
+  ros::ServiceServer delete_vehicle = nh.advertiseService("delete_vehicle", &VehicleManagerServer::delete_vehicle, this);
+
   ROS_INFO("Vehicle Server Started");
 
   ros::spin();
