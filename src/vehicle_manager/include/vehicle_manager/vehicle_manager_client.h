7917d9f05492c1a791dd295b1b0e6ee6d62aab3e
JoshuaRVLS <jravaellnew@gmail.com>
2025-10-25 09:15:22 +0700

    Add service clients and methods for editing and deleting vehicles in VehicleManagerClient

--- src/vehicle_manager/include/vehicle_manager/vehicle_manager_client.h
+++ src/vehicle_manager/include/vehicle_manager/vehicle_manager_client.h
@@ -4,6 +4,8 @@
 #include "vehicle_manager/RegisterVehicle.h"
 #include "vehicle_manager/FindVehicle.h"
 #include "vehicle_manager/DisplayAllVehicle.h"
+#include "vehicle_manager/EditVehicle.h"
+#include "vehicle_manager/DeleteVehicle.h"
 #include <iostream>
 
 
@@ -14,6 +16,8 @@ class VehicleManagerClient {
   ros::ServiceClient register_vehicle_client;
   ros::ServiceClient find_vehicle;
   ros::ServiceClient display_all_vehicles;
+  ros::ServiceClient delete_vehicle_client;
+  ros::ServiceClient edit_vehicle_client;
 
   void show_menu();
 
@@ -22,6 +26,9 @@ class VehicleManagerClient {
   void add_vehicle_menu();
   void find_vehicle_menu();
   void display_vehicles_menu();
+  void print_info(vehicle_manager::Vehicle& vehicle);
+  void delete_vehicle(const int& vehicle_id);
+  void update_vehicle(const std::string& property_name, const int& vehicle_id, const std::string& new_value);
 
 public:
   VehicleManagerClient();
